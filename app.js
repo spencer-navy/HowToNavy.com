9a18f18161b8556120c1f9f22fdee89c11dab5ca
Spencer <78234961+spencer-navy@users.noreply.github.com>
2022-01-01 04:44:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.body.querySelector(".menuIcon").addEventListener("click",function(){
     if (toggle){
         document.body.querySelector(".menu").style.right="0";
     }else{
-        document.body.querySelector(".menu").style.right="-150px";
+        document.body.querySelector(".menu").style.right="-160px";
     }
 
 })
